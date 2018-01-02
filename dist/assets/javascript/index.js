ccb9507b605cbe9ddd52762df3678784e35a6677
cbtelemann <hello@telemann.ink>
2018-01-01 17:19:29 -0800

    update

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -7,8 +7,8 @@ $(function () {
   if (location.pathname !== '/') {
     $('.uk-navbar-item > a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('underline');
   }
-  if (location.pathname == '/') {
-    $('.uk-navbar-item > a[href="/"]').addClass('underline');
-  }
+  // if (location.pathname == '/') {
+  //   $('.uk-navbar-item > a[href="/"]').addClass('underline');
+  // }
 });
 //# sourceMappingURL=index.js.map
