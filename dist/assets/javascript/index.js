d5ac033b941be2b57eedd19145e15be7395b8748
cbtelemann <hello@telemann.ink>
2018-01-13 16:23:14 -0800

    update

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -11,4 +11,20 @@ $(function () {
   //   $('.uk-navbar-item > a[href="/"]').addClass('underline');
   // }
 });
+
+jQuery("#mc-embedded-subscribe-form").submit(function (e) {
+
+  var url = jQuery(this).prop('action'); // the script where you handle the form input.
+  jQuery.ajax({
+    type: "GET",
+    url: url,
+    data: jQuery("#mc-embedded-subscribe-form").serialize(), // serializes the form's elements.
+    dataType: "jsonp",
+    success: function success(data) {
+      if (data.result == 'success') window.location.href = 'http://localhost:3000/thank-you';
+    }
+  });
+
+  e.preventDefault(); // avoid to execute the actual submit of the form.
+});
 //# sourceMappingURL=index.js.map
