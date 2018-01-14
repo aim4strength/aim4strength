853c064bb449ba5697c4020230b2b5b469eb76da
cbtelemann <hello@telemann.ink>
2018-01-14 09:46:58 -0800

    update mailchimp form - ajax

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -12,19 +12,20 @@ $(function () {
   // }
 });
 
-jQuery("#mc-embedded-subscribe-form").submit(function (e) {
-
-  var url = jQuery(this).prop('action'); // the script where you handle the form input.
-  jQuery.ajax({
-    type: "GET",
-    url: url,
-    data: jQuery("#mc-embedded-subscribe-form").serialize(), // serializes the form's elements.
-    dataType: "jsonp",
-    success: function success(data) {
-      if (data.result == 'success') window.location.href = 'http://localhost:3000/thank-you';
-    }
-  });
-
-  e.preventDefault(); // avoid to execute the actual submit of the form.
-});
+// jQuery("#mc-embedded-subscribe-form").submit(function(e) {
+//
+//     var url = jQuery(this).prop('action'); // the script where you handle the form input.
+//     jQuery.ajax({
+//            type: "GET",
+//            url: url,
+//            data: jQuery("#mc-embedded-subscribe-form").serialize(), // serializes the form's elements.
+//            dataType: "jsonp",
+//            success: function(data)
+//            {
+//                 if(data.result == 'success') window.location.href = 'http://localhost:3000/thank-you';
+//            }
+//          });
+//
+//     e.preventDefault(); // avoid to execute the actual submit of the form.
+// });
 //# sourceMappingURL=index.js.map
